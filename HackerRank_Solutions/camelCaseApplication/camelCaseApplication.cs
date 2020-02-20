349966be411be4165a604d15c3b24427b5c4b9ef
Ananda <8168596+oneananda@users.noreply.github.com>
2020-02-20 09:23:08 +0530

    Adding Hacker Rank Solutions
    Adding Hacker Rank Solutions

---
+++ HackerRank_Solutions/camelCaseApplication/camelCaseApplication.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace camelCaseApplication
+{
+    class Program
+    {
+        // Complete the camelcase function below.
+        static int camelcase(string s)
+        {
+            char[] charArr = s.ToCharArray();
+            List<int> whereToSeperate = new List<int>();
+            int charCounter = 0;
+            foreach (char singleChar in charArr)
+            {
+                if (char.IsUpper(singleChar))
+                    whereToSeperate.Add(charCounter);
+                charCounter++;
+            }
+            return whereToSeperate.Count() + 1;
+        }
+
+        static void Main(string[] args)
+        {
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string s = Console.ReadLine();
+
+            int result = camelcase(s);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+            //textWriter.WriteLine(result);
+
+            //textWriter.Flush();
+            //textWriter.Close();
+        }
+    }
+}
