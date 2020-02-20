2f2030e625b0573967a938390cf8dbc31da1cf82
Ananda <8168596+oneananda@users.noreply.github.com>
2020-02-20 14:40:18 +0530

    Count Number of words in a given string
    Use : StringComparison.InvariantCultureIgnoreCase

---
+++ Miscellaneous/String Manipulation/Count_Number_of_Words/CountWord.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class CountWord : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnShowResult_Click(object sender, EventArgs e)
+    {
+        string givenString = txtInputString.Text.Trim();
+
+        StringBuilder StrResult = new StringBuilder();
+
+        List<string> Str1 = givenString.Split(' ').ToList();
+
+        Hashtable HtUniqeString = new Hashtable();
+
+        foreach (string SingleStr1 in Str1)
+        {
+            if (SingleStr1.Trim() != string.Empty)// Excluding any extra spaces
+            {
+                string TempString = SingleStr1.Replace(',', ' ');// Remoce any commas
+                if (!HtUniqeString.ContainsKey(TempString.Trim()))
+                {
+                    HtUniqeString.Add(SingleStr1, "");
+                }
+            }
+        }
+        foreach (DictionaryEntry DictEntry in HtUniqeString)
+        {
+            int Count = 0;
+            foreach (string SingleStr1 in Str1)
+            {
+                // Compare the strings irrespective of case
+                if (SingleStr1.Equals(DictEntry.Key.ToString(), StringComparison.InvariantCultureIgnoreCase))
+                    Count++;
+            }
+            string Time = "Time";
+            if (Count > 1) Time = "Times";
+            StrResult.AppendLine("'" + DictEntry.Key.ToString() + "' has occured " + Count.ToString() + " " + Time + " ,");
+            //_CountWordModel.OutputMessages.Add("'" + DictEntry.Key.ToString() + "' has occured " + Count.ToString() + " " + Time);
+            //Console.WriteLine(DictEntry.Key.ToString() + " has occured " + Count.ToString() + " times");
+        }
+
+        lblResult.Text = StrResult.ToString();
+    }
+}
---
+++ Miscellaneous/String Manipulation/Count_Number_of_Words/Readme.md
@@ -0,0 +1,10 @@
+Coding Challenge - ASP.NET Program : Count number of occurrences in a given string with additional criteria - StringComparison.InvariantCultureIgnoreCase
+
+Write a ASP.NET with C# program to achieve the following
+
+Given a sentence, count each word for number of occurrences (Main Criteria: No repetition)
+
+Additional Criteria 
+Case should be ignored
+Comma should not be counted
+An extra space more than single space should be ignored
