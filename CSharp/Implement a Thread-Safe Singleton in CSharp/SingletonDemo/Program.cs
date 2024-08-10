3fcabafce41d35fc6461bcea2a7f7491241fe78f
oneananda <anandathirumurugan@gmail.com>
2024-08-10 13:08:42 +0530

    Singleton demo for Lazy Initialization

---
+++ CSharp/Implement a Thread-Safe Singleton in CSharp/SingletonDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace SingletonDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TestSingleton();
+            TestSingleton();
+            // Calling mutiple times resulting the same hash code is being displayed
+
+            // Going for parallel invoke
+
+            Parallel.Invoke(
+                ()=>TestSingleton(),
+                ()=>TestSingleton(),
+                ()=>TestSingleton());
+
+            Console.ReadLine();
+        }
+
+        private static void TestSingleton()
+        {
+            Singleton singleton = Singleton.GetInstance();
+            Console.WriteLine(singleton.GetHashCode());
+        }
+    }
+}
---
+++ CSharp/Implement a Thread-Safe Singleton in CSharp/SingletonDemo/Singleton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    public sealed class Singleton
+    {
+        private static Singleton instance = null;
+
+        private static readonly object lockObj = new object();
+
+        // Private constructor to prevent instantiation from outside.
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                lock (lockObj)
+                {
+                    // Lazy Initialization: The Singleton instance is created only when
+                    // it is needed for the first time, not at the time of class loading.
+                    // This is achieved by checking if instance is null before creating it.
+                    instance = new Singleton();
+                }
+            }
+            return instance;
+        }
+    }
+}
