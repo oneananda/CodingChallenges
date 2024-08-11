d2145b7d4daf05cdc8325dd6bab9c5b8e001ca04
oneananda <anandathirumurugan@gmail.com>
2024-08-11 07:09:17 +0530

    Adding Eager initialization

---
+++ CSharp/Implement a Thread-Safe Singleton in CSharp/Eager_Initialization_Singleton/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Eager_Initialization_Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp/Implement a Thread-Safe Singleton in CSharp/Eager_Initialization_Singleton/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eager_Initialization_Singleton
+{
+    // 'sealed' prevent it from being inherited.
+    // If the Singleton class were not sealed, it could be subclassed
+    public sealed class Singleton
+    {
+        // The Singleton instance is created when the class is loaded.
+        private static readonly Singleton instance = new Singleton();
+
+        // Private constructor to prevent instantiation from outside.
+        private Singleton()
+        {
+            // Initialize any resources here if needed.
+        }
+
+        // Public method to provide global access to the instance.
+        public static Singleton GetInstance()
+        {
+            return instance;
+        }
+    }
+
+}
